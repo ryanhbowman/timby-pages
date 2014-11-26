a51b805d0340e61cc51e9350f2df786801230c12
Ryan Bowman <ryan@circle.co.ke>
2014-11-26 16:10:47 +0000

    huh?

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,14 +1,31 @@
-function activateImage(){
-  var images = $('.responsive-image');
-  $.each(images, function(index, val) {
-     $string = $('.responsive-image').attr('data-src');
-     $(this).attr('src', $string);
+$( document ).ready(function() {
+  $('#menu_control').click(function(event) {
+      $('.mobile-menu').toggleClass('active-menu');
+      $('#menu_control').toggleClass('active');
   });
-};
+  $features = $('.page-header__features');
+  if ($features.length > 0){
+    $featuresHeight = $features.innerHeight();
+  }
+});
+$(window).scroll(function(event) {
+  $scrolled = $(window).scrollTop();
+  if ($features.length > 0 && $scrolled > $featuresHeight-160){
+    $('.features-menu-scroll').addClass('is-active');
+  }
+  else {
+    $('.features-menu-scroll').removeClass('is-active');
+  }
+  // if scrolled past main header, show the little header
+  if ($scrolled > 140) {
+    $('.header').addClass('is-active');
+    // $('.glasses-grid').addClass('fixed')
+  }
+  //otherwise dont show that stuff
+  else  {
+    $('.header').removeClass('is-active'); 
+  }
+});
+$( window ).resize(function() {
 
-$(document).ready(function(){
-  // $string = $('.responsive-image').attr('data-src');
-    if (window.matchMedia("(min-width: 45em)").matches) {
-      activateImage();
-    }
 });
