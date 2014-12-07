e3a9bd326f0a2832c391119efa516a37baf225ae
Ryan Bowman <ryan@circle.co.ke>
2014-12-07 13:10:51 +0000

    finished story section, finished submenu for features

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 $( document ).ready(function() {
+$('#reportinglink').click(function(e) {
+  e.preventDefault();
+  $('html,body').animate({scrollTop:$('#reporting').offset().top - 130});
+});
+$('#dashboardlink').click(function(e) {
+  e.preventDefault();
+  $('html,body').animate({scrollTop:$('#dashboard').offset().top - 145});
+});
+$('#storylink').click(function(e) {
+  e.preventDefault();
+  $('html,body').animate({scrollTop:$('#story').offset().top - 145});
+});
+
  $('.various').fancybox({
     openEffect  : 'none',
     closeEffect : 'none'
@@ -15,12 +28,6 @@ $( document ).ready(function() {
 });
 $(window).scroll(function(event) {
   $scrolled = $(window).scrollTop();
-  if ($features.length > 0 && $scrolled > $featuresHeight-160){
-    $('.features-menu-scroll').addClass('is-active');
-  }
-  else {
-    $('.features-menu-scroll').removeClass('is-active');
-  }
   // if scrolled past main header, show the little header
   if ($scrolled > 140) {
     $('.header').addClass('is-active');
@@ -30,6 +37,36 @@ $(window).scroll(function(event) {
   else  {
     $('.header').removeClass('is-active'); 
   }
+
+  
+  if ($('#reporting').length > 0){
+    $reportingHeight = $('#reporting').innerHeight();
+    $dashboardHeight = $('#dashboard').innerHeight();
+    $storyHeight = $('#story').innerHeight();
+  }
+  if ($scrolled > $featuresHeight + $reportingHeight - 180 && $scrolled < $featuresHeight + $reportingHeight + $dashboardHeight -150){
+    $('.features-item').removeClass('is-happening');
+    $('#dashboardlink').addClass('is-happening');
+    console.log('dashboard');
+  }
+  else if ($scrolled > $featuresHeight + $reportingHeight + $dashboardHeight - 150) {
+    $('.features-item').removeClass('is-happening');
+    $('#storylink').addClass('is-happening');
+    console.log('story');
+  }
+  else {
+    $('.features-item').removeClass('is-happening');
+    $('#reportinglink').addClass('is-happening');
+    console.log('reporting');
+  }
+
+  if ($features.length > 0 && $scrolled > $featuresHeight-160){
+    $('.features-menu-scroll').addClass('is-active');
+  }
+  else {
+    $('.features-menu-scroll').removeClass('is-active');
+  }
+  
 });
 $( window ).resize(function() {
 
